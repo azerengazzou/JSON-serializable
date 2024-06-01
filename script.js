ec10df05cf8035b4dc0ede35aa5ea0d71938da91
azer.engazzou <azer.engazzou@corilus.be>
2024-06-02 00:45:55 +0100

    add image

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const fileId = "11E92qStOB2JA6vSfWwHknKlZwucFtNon";
-    const apiKey = "AIzaSyApcQVyEBjqvVC8YdjVmcfn_phTWfcG510";
-    const url = `https://www.googleapis.com/drive/v3/files/11E92qStOB2JA6vSfWwHknKlZwucFtNon?alt=media&key=AIzaSyApcQVyEBjqvVC8YdjVmcfn_phTWfcG510
-    `;
+    const fileId = "";
+    const apiKey = "";
+    const url = `https://www.googleapis.com/drive/v3/files/${fileId}?alt=media&key=${apiKey}`;
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const jsonData = document.getElementById("jsonData");
-            jsonData.textContent = JSON.stringify(data, null, 2);
-        })
-        .catch(error => {
-            console.error("Error fetching JSON data:", error);
-        });
-
-        const jsonData = {
-            "employee": {
-                "name": "sonoo",
-                "salary": 56000,
-                "married": true
-            }
-        };
+    fetch(url, { mode: 'no-cors' })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
         const nameElement = document.getElementById("name");
-    const salaryElement = document.getElementById("salary");
-    const marriedElement = document.getElementById("married");
+        const salaryElement = document.getElementById("salary");
+        const marriedElement = document.getElementById("married");
+        const imageElement = document.getElementById("image");
 
-    nameElement.textContent = jsonData.employee.name;
-    salaryElement.textContent = "Salary: $" + jsonData.employee.salary;
-    marriedElement.textContent = "Married: " + (jsonData.employee.married ? "Yes" : "No");
+        console.log(data.employee.image);
+        nameElement.textContent = data.employee.name;
+        salaryElement.textContent = "Salary: $" + data.employee.salary;
+        marriedElement.textContent = "Married: " + (data.employee.married ? "Yes" : "No");
+        imageElement.src = data.employee.image;
+    })
+    .catch(error => {
+        console.error("Error fetching JSON data:", error.Error);
+    });
 });
